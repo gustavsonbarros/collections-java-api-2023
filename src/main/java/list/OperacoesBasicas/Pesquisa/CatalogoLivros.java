a515feec4e8437933538951d2dea5b159d3e4c02
gustavsonbarros <gustavson.adm@gmail.com>
2025-03-29 18:00:33 -0300

    Resolvendo exercicio CatalogoLivros e Ordenacao
    Signed-off-by: gustavsonbarros <gustavson.adm@gmail.com>

---
+++ src/main/java/list/OperacoesBasicas/Pesquisa/CatalogoLivros.java
@@ -0,0 +1,68 @@
+package main.java.list.OperacoesBasicas.Pesquisa;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CatalogoLivros {
+    private List<Livro> livroList;
+
+    public CatalogoLivros() { this.livroList = new ArrayList<>();}
+
+    public void adicionarLivro(String titulo, String autor, int anoPublicacao){
+        livroList.add(new Livro(titulo, autor, anoPublicacao));
+    }
+
+    public List<Livro> pesquisarPorAutor(String autor) {
+        List<Livro> livrosPorAutor = new ArrayList<>();
+        if (!livroList.isEmpty()) {
+            livrosPorAutor = new ArrayList<>();
+            for (Livro l : livroList) {
+                if (l.getAutor().equalsIgnoreCase(autor)) {
+                    livrosPorAutor.add(l);
+                }
+            }
+        }
+        return livrosPorAutor;
+    }
+
+    public List<Livro> pesquisarPorIntervaloAnos(int anoInicial, int anoFinal){
+        List<Livro> livrosPorIntervaloAnos = new ArrayList<>();
+        if (!livroList.isEmpty()) {
+            for(Livro l: livroList){
+                if (l.getAnopublicacao() >= anoInicial && l.getAnopublicacao() <= anoFinal) {
+                    livrosPorIntervaloAnos.add(l);
+                }
+            }
+        }
+        return livrosPorIntervaloAnos;
+
+    }
+
+    public Livro pesquisarPorTitulo(String titulo){
+        Livro livroPorTitulo = null;
+        if(!livroList.isEmpty()){
+            for(Livro l: livroList){
+                if(l.getTitulo().equalsIgnoreCase(titulo)){
+                    livroPorTitulo = l;
+                    break;
+                }
+            }
+        }
+        return livroPorTitulo;
+    }
+    public static void main(String[] args){
+        CatalogoLivros catalogoLivros = new CatalogoLivros();
+        catalogoLivros.adicionarLivro("Microsserviços Prontos Para a Produção", "Susan J. Fowler", 2017);
+        catalogoLivros.adicionarLivro("Java Guia do Programador", "Peter Jandl Junior", 2021);
+        catalogoLivros.adicionarLivro("Código Limpo", "Robert C. Martin", 2009);
+        catalogoLivros.adicionarLivro("O Codificador Limpo", "Robert C. Martin", 2012);
+
+        System.out.println(catalogoLivros.pesquisarPorAutor("Peter Jandl Junior"));
+        System.out.println(catalogoLivros.pesquisarPorIntervaloAnos(2009, 2017));
+
+        System.out.println(catalogoLivros.pesquisarPorTitulo("Código Limpo"));
+
+    }
+
+
+}
