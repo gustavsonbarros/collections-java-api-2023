5c185ba3f6e496baba0e367df7d3d7a92b020cab
gustavsonbarros <gustavson.adm@gmail.com>
2025-03-29 21:09:15 -0300

    Resolvendo desafio Set
    Signed-off-by: gustavsonbarros <gustavson.adm@gmail.com>

---
+++ src/main/java/list/OperacoesBasicas/OperacoesBasicas/ConjuntoConvidados.java
@@ -0,0 +1,52 @@
+package main.java.list.OperacoesBasicas.OperacoesBasicas;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class ConjuntoConvidados {
+    private Set<Convidado> convidadoSet;
+
+    public ConjuntoConvidados() {
+        this.convidadoSet = new HashSet<>();
+    }
+
+    public void adicionarConvidado(String nome, int codigoConvite){
+        convidadoSet.add(new Convidado(nome, codigoConvite));
+    }
+
+    public void removerConvidadoPorCodigoConvite(int codigoConvite){
+        Convidado convidadoParaRemover = null;
+        for(Convidado c: convidadoSet){
+            if(c.getCodigoConvite() == codigoConvite){
+                convidadoParaRemover = c;
+                break;
+            }
+
+        }
+        convidadoSet.remove(convidadoParaRemover);
+
+    }
+    public int contarConvidado(){
+        return convidadoSet.size();
+    }
+
+    public void exibirConvidados(){
+        System.out.println(convidadoSet);
+    }
+
+    public static void main(String[] args){
+        ConjuntoConvidados conjuntoConvidados = new ConjuntoConvidados();
+        System.out.println("Existem " + conjuntoConvidados.contarConvidado() + " convidados dentro do Set Convidado");
+
+        conjuntoConvidados.adicionarConvidado("Alice", 1234);
+        conjuntoConvidados.adicionarConvidado("Bob", 1235);
+        conjuntoConvidados.adicionarConvidado("Charlie", 1235);
+        conjuntoConvidados.adicionarConvidado("David", 1236);
+
+        System.out.println("Convidados no conjunto:");
+        conjuntoConvidados.exibirConvidados();
+
+        System.out.println("Existem " + conjuntoConvidados.contarConvidado() + " convidado(s) dentro do Set Convidado");
+
+    }
+}
